a759fb8f86e3c73fc61c4efbd1d874e864f363bd
Caske <vdlucas18@gmail.com>
2015-06-18 19:53:32 +0200

    Trying The APIs

---
+++ src/main/java/com/caske2000/caskearmor/CaskeArmor.java
@@ -0,0 +1,30 @@
+package com.caske2000.caskearmor;
+
+import com.caske2000.caskearmor.lib.Reference;
+import cpw.mods.fml.common.Mod;
+import cpw.mods.fml.common.event.FMLInitializationEvent;
+import cpw.mods.fml.common.event.FMLPostInitializationEvent;
+import cpw.mods.fml.common.event.FMLPreInitializationEvent;
+
+@Mod(modid = Reference.MODID, name = Reference.NAME, version = Reference.ID)
+public class CaskeArmor
+{
+
+    @Mod.EventHandler
+    public void preInit(FMLPreInitializationEvent preInit)
+    {
+
+    }
+
+    @Mod.EventHandler
+    public void init(FMLInitializationEvent preInit)
+    {
+
+    }
+
+    @Mod.EventHandler
+    public void postInit(FMLPostInitializationEvent preInit)
+    {
+
+    }
+}
---
+++ src/main/java/com/caske2000/caskearmor/lib/Reference.java
@@ -0,0 +1,8 @@
+package com.caske2000.caskearmor.lib;
+
+public class Reference
+{
+    public static final String MODID = "caskearmor";
+    public static final String NAME = "Caske Armor";
+    public static final String ID = "1.7.10-1.0";
+}
